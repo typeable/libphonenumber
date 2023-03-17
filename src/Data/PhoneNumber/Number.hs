4cb8c2f68d3d8f8703762bd7c0dff45f75ba1cef
mniip <mniip@mniip.com>
2023-03-17 12:40:18 +0100

    Relax restrictions for record pattern synonyms
    I'm not sure where I got 9.2+ from

--- src/Data/PhoneNumber/Number.hs
+++ src/Data/PhoneNumber/Number.hs
@@ -44,8 +44,8 @@ newtype CountryCode = CountryCode Int
   deriving newtype (Eq, Ord, Show, Num)
 
 -- | Record pattern synonym for accessing data fields of the underlying C++
--- object. If on GHC 9.2+, it can be used for record construction, record
--- update, and record pattern match. See t'PhoneNumber'.
+-- object. It can be used for record construction, record update, and record
+-- pattern match. See t'PhoneNumber'.
 pattern PhoneNumber
   :: Maybe ByteString
   -> Maybe ByteString
@@ -57,7 +57,6 @@ pattern PhoneNumber
   -> Maybe Int
   -> PhoneNumber
 pattern PhoneNumber
-#if __GLASGOW_HASKELL__ >= 902
   { extension
   , rawInput
   , preferredDomesticCarrierCode
@@ -67,10 +66,6 @@ pattern PhoneNumber
   , countryCodeSource
   , numberOfLeadingZeros
   }
-#else
-  extension rawInput preferredDomesticCarrierCode nationalNumber countryCode
-  italianLeadingZero countryCodeSource numberOfLeadingZeros
-#endif
   <- (toCPhoneNumber -> CPhoneNumber
     { I.nationalNumber = fromIntegral -> nationalNumber
     , I.countryCode = fromIntegral -> CountryCode -> countryCode
@@ -82,31 +77,5 @@ pattern PhoneNumber
       (CountryCode (fromIntegral -> countryCode)) italianLeadingZero
       countryCodeSource (fmap fromIntegral -> numberOfLeadingZeros)
         = fromCPhoneNumber $ CPhoneNumber{..}
-
-#if __GLASGOW_HASKELL__ < 902
-extension :: PhoneNumber -> Maybe ByteString
-extension (PhoneNumber x _ _ _ _ _ _ _) = x
-
-rawInput :: PhoneNumber -> Maybe ByteString
-rawInput (PhoneNumber _ x _ _ _ _ _ _) = x
-
-preferredDomesticCarrierCode :: PhoneNumber -> Maybe ByteString
-preferredDomesticCarrierCode (PhoneNumber _ _ x _ _ _ _ _) = x
-
-nationalNumber :: PhoneNumber -> Word
-nationalNumber (PhoneNumber _ _ _ x _ _ _ _) = x
-
-countryCode :: PhoneNumber -> CountryCode
-countryCode (PhoneNumber _ _ _ _ x _ _ _) = x
-
-italianLeadingZero :: PhoneNumber -> Maybe Bool
-italianLeadingZero (PhoneNumber _ _ _ _ _ x _ _) = x
-
-countryCodeSource :: PhoneNumber -> Maybe CountryCodeSource
-countryCodeSource (PhoneNumber _ _ _ _ _ _ x _) = x
-
-numberOfLeadingZeros :: PhoneNumber -> Maybe Int
-numberOfLeadingZeros (PhoneNumber _ _ _ _ _ _ _ x) = x
-#endif
 
 {-# COMPLETE PhoneNumber #-}

c84e1fa44dff4fd2733e5be583ace3da9f9228c8
mniip <mniip@mniip.com>
2023-03-22 13:14:28 +0100

    Add haddocks for PhoneNumber selectors

--- src/Data/PhoneNumber/Number.hs
+++ src/Data/PhoneNumber/Number.hs
@@ -79,3 +79,68 @@ pattern PhoneNumber
         = fromCPhoneNumber $ CPhoneNumber{..}
 
 {-# COMPLETE PhoneNumber #-}
+
+-- | E.g.:
+--
+-- @
+-- 'Data.PhoneNumber.Util.parseNumber' 'Data.PhoneNumber.Util.Canonicalize' 'Nothing' "+1 800-234-5678 ext. 1234"
+-- = 'Right' v'PhoneNumber' { 'extension' = 'Just' "1234", .. }
+-- @
+extension :: PhoneNumber -> Maybe ByteString
+
+-- | E.g.:
+--
+-- @
+-- 'Data.PhoneNumber.Util.parseNumber' 'Data.PhoneNumber.Util.KeepRawInput' 'Nothing' " + 1(2-3~4*5.6 "
+-- = 'Right' v'PhoneNumber' { 'rawInput' = 'Just' " + 1(2-3~4*5.6 ", .. }
+-- @
+rawInput :: PhoneNumber -> Maybe ByteString
+
+-- | E.g.:
+--
+-- @
+-- 'Data.PhoneNumber.Util.parseNumber' 'Data.PhoneNumber.Util.KeepRawInput' ('Just' \"BR\") "0 41 (21) 2345-6789"
+-- = 'Right' v'PhoneNumber' { 'preferredDomesticCarrierCode' = 'Just' "41", .. }
+-- @
+preferredDomesticCarrierCode :: PhoneNumber -> Maybe ByteString
+
+-- | You probably want to use 'Data.PhoneNumber.Util.nationalSignificantNumber'
+-- instead. E.g.:
+--
+-- @
+-- 'Data.PhoneNumber.Util.parseNumber' 'Data.PhoneNumber.Util.Canonicalize' 'Nothing' "+800 0001 2345"
+-- = 'Right' v'PhoneNumber' { 'nationalNumber' = 12345, 'numberOfLeadingZeros' = Just 3, .. }
+-- @
+nationalNumber :: PhoneNumber -> Word
+
+-- | E.g.:
+--
+-- @
+-- 'Data.PhoneNumber.Util.parseNumber' 'Data.PhoneNumber.Util.Canonicalize' ('Just' \"US\") "800-234-5678"
+-- = 'Right' v'PhoneNumber' { 'countryCode' = 1, .. }
+-- @
+countryCode :: PhoneNumber -> CountryCode
+
+-- | E.g.:
+--
+-- @
+-- 'Data.PhoneNumber.Util.parseNumber' 'Data.PhoneNumber.Util.Canonicalize' 'Nothing' "+39 06 1234 5678"
+-- = 'Right' v'PhoneNumber' { 'italianLeadingZero' = 'Just' 'True', .. }
+-- @
+italianLeadingZero :: PhoneNumber -> Maybe Bool
+
+-- | E.g.:
+--
+-- @
+-- 'Data.PhoneNumber.Util.parseNumber' 'Data.PhoneNumber.Util.KeepRawInput' ('Just' \"US\") "011 800 1234 5678"
+-- = 'Right' v'PhoneNumber' { 'countryCodeSource' = 'Just' 'FromNumberWithIdd', .. }
+-- @
+countryCodeSource :: PhoneNumber -> Maybe CountryCodeSource
+
+-- | E.g.:
+--
+-- @
+-- 'Data.PhoneNumber.Util.parseNumber' 'Data.PhoneNumber.Util.Canonicalize' 'Nothing' "+800 0001 2345"
+-- = 'Right' v'PhoneNumber' { 'numberOfLeadingZeros' = Just 3, 'nationalNumber' = 12345, .. }
+-- @
+numberOfLeadingZeros :: PhoneNumber -> Maybe Int
